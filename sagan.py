ce75373f839e57b45f2452f9f5875429bff7a258
Chandler Supple <36590015+chandlersupple@users.noreply.github.com>
2018-11-06 21:31:01 -0800

    Update sagan.py

--- sagan.py
+++ sagan.py
@@ -13,9 +13,10 @@ sess = tf.InteractiveSession()
 epochs = 128
 batch_size = 64
 g_lr = 0.0001
-d_lr = 0.0004
+d_lr = 0.0001
 beta1 = 0.0
 beta2 = 0.9
+lambd = 1 / 4
 
 # Attention Mechanism
 def attention(x, channels, scope):
@@ -23,9 +24,9 @@ def attention(x, channels, scope):
     with tf.variable_scope(scope, reuse= tf.AUTO_REUSE):
         
         # f, g, and h Weight Matrices -- 1x1 Convolutions
-        f = conv(x, channels // 8, kernel_size= 1, strides= 1, pad= 0, scope= 'f_conv')
-        g = conv(x, channels // 8, kernel_size= 1, strides= 1, pad= 0, scope= 'g_conv')
-        h = conv(x, channels, kernel_size= 1, strides= 1, pad= 0, scope= 'h_conv')
+        f = conv(x, channels // 8, kernel_size= 1, strides= 1, scope= 'f_conv')
+        g = conv(x, channels // 8, kernel_size= 1, strides= 1, scope= 'g_conv')
+        h = conv(x, channels, kernel_size= 1, strides= 1, scope= 'h_conv')
         
         s = tf.matmul(flatten(g), flatten(f), transpose_b= True)
         beta = tf.nn.softmax(s, axis= -1)
@@ -77,33 +78,34 @@ def discriminator(x, is_training= True):
         
         conv1 = conv(x, 32, scope= 'conv1')
         conv1 = batch_normalization(conv1, is_training= is_training)
-        conv1 = tf.nn.leaky_relu(conv1, alpha= 0.1)
+        conv1 = tf.nn.leaky_relu(conv1)
         
         conv2 = conv(conv1, 64, scope= 'conv2')
         conv2 = batch_normalization(conv2, is_training= is_training)
-        conv2 = tf.nn.leaky_relu(conv2, alpha= 0.1)
+        conv2 = tf.nn.leaky_relu(conv2)
         
         att_conv2 = attention(conv2, 64, 'conv2_att')
         
         conv3 = conv(att_conv2, 128, scope= 'conv3')
         conv3 = batch_normalization(conv3, is_training= is_training)
-        conv3 = tf.nn.leaky_relu(conv3, alpha= 0.1)
+        conv3 = tf.nn.leaky_relu(conv3)
 
         conv4 = conv(conv3, 256, scope= 'conv4')
         conv4 = batch_normalization(conv4, is_training= is_training)
-        conv4 = tf.nn.leaky_relu(conv4, alpha= 0.1)
+        conv4 = tf.nn.leaky_relu(conv4)
         
         conv5 = conv(conv4, 512, scope= 'conv5')
         conv5 = batch_normalization(conv5, is_training= is_training)
-        conv5 = tf.nn.leaky_relu(conv5, alpha= 0.1)
+        conv5 = tf.nn.leaky_relu(conv5)
 
         flatt_conv5 = tf.layers.flatten(conv5)
-        dl1 = tf.layers.dense(flatt_conv5, 1)
+        dl1 = dense(flatt_conv5, 1)
         
         return dl1
             
 # Placeholders
 x = tf.placeholder(tf.float32, [batch_size, 128, 128, 3])
+x_pert = tf.placeholder(tf.float32, [batch_size, 128, 128, 3])
 z = tf.placeholder(tf.float32, [batch_size, 128])
 it = tf.placeholder(tf.bool)
 
@@ -112,11 +114,20 @@ gf = generator(z, is_training= it)
 df = discriminator(gf, is_training= it)
 
 # Loss
-r_loss = tf.reduce_mean(tf.nn.relu(1 - dr))
-f_loss = tf.reduce_mean(tf.nn.relu(1 + df))
+r_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits= dr, labels= tf.ones_like(dr)))
+f_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits= df, labels= tf.zeros_like(df)))
 d_loss = tf.add(r_loss, f_loss)
 
-g_loss = -1 * tf.reduce_mean(df)
+alpha = tf.random_uniform(shape= (x).get_shape().as_list(), minval= 0.0, maxval= 1.0)
+x_diff = x_pert - x
+interpolate = x + (alpha * x_diff)
+disc_out = discriminator(interpolate, is_training= True)
+grads = tf.gradients(disc_out, [interpolate])[0]
+slope = tf.sqrt(tf.reduce_sum(tf.square(grads), reduction_indices= [1]))
+gradient_penalty = tf.reduce_mean((slope - 1.0) ** 2)
+d_loss = d_loss + (lambd * gradient_penalty)
+
+g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits= df, labels= tf.ones_like(df)))
 
 t_vars = tf.trainable_variables()
 d_vars = [var for var in t_vars if 'd' in var.name]
@@ -128,29 +139,30 @@ g_opt = tf.train.AdamOptimizer(g_lr, beta1= beta1, beta2= beta2).minimize(g_loss
         
 sess.run(tf.global_variables_initializer())
 
-batches_in_epoch = 20000 // batch_size # '20000' refers to the number of training images
+batches_in_epoch = 20000 // batch_size # Where 20000 == Number of training images
 images = []
 
 # Checkpoints
 saver = tf.train.Saver()
-save_dir = '/Users/Masterchief7269/Desktop/Programming Header/Python/Models/SAGAN/' # Change accordingly
+save_dir = '/Users/Masterchief7269/Desktop/Programming Header/Python/Models/SAGAN/'
 
 # Training
 for epoch_iter in range (epochs):
     for batch_iter in range (1, batches_in_epoch):
         b_x = ret_data(batch_size, batch_iter)
         b_z = np.random.uniform(-1.0, 1.0, size= [batch_size, 128])
-        sess.run(d_opt, feed_dict= {x: b_x, z: b_z, it: True})
-        sess.run(g_opt, feed_dict= {x: b_x, z: b_z, it: True})
+        x_p = ret_pert(b_x)
+        sess.run(d_opt, feed_dict= {x: b_x, x_pert: x_p, z: b_z, it: True})
+        sess.run(g_opt, feed_dict= {x: b_x, x_pert: x_p, z: b_z, it: True})
         
         if batch_iter % 15 == 0:
             gl = sess.run(g_loss, feed_dict= {z: b_z, it: False})
-            dl = sess.run(d_loss, feed_dict= {x: b_x, z: b_z, it: False})
+            dl = sess.run(d_loss, feed_dict= {x: b_x, x_pert: x_p, z: b_z, it: False})
             
             print('Epoch %s, Batch %s / %s, D-loss: %s, G-loss: %s' %(epoch_iter, batch_iter, batches_in_epoch, dl, gl))
             
         if batch_iter % 25 == 0:
-            g_img = sess.run(gf, feed_dict= {x: b_x, z: b_z, it: False})
+            g_img = sess.run(gf, feed_dict= {x: b_x, x_pert: x_p, z: b_z, it: False})
             images.append(g_img)
 
         if batch_iter % 100 == 0:
